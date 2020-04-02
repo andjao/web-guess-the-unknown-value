f16d25e5eab437354c63bacbc72a84a6bbbd2a1d
andjao <ads.marchi@gmail.com>
2020-04-02 20:52:41 -0300

    example

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ document.addEventListener("click", function (e) {
         expressionE = ' ? ';
         document.getElementById('valueFinal').value = '';
         document.getElementById('valueFinal').focus();
-        document.getElementById('expression').innerHTML = 'Exemplo: ⚽ × ⚽ - ⚽ = 6';
+        document.getElementById('expression').innerHTML = 'Exemplo:<br>⚽ × ⚽ - ⚽ = 6<br>3 × 3 - 3 = 6';
         document.getElementById('resultP').innerHTML = '';
         document.getElementById('resultN').innerHTML = '';
     }
